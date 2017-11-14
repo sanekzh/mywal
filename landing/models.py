1f61c230b8c7ede139d8a52344a76ddce4f7598f
sanek <aksm.sanek@gmail.com>
2017-11-15 01:00:27 +0200

    12.11.2017 import and export file, delete row in table

--- landing/models.py
+++ landing/models.py
@@ -3,13 +3,9 @@ from django.db import models
 
 
 class Subscriber(models.Model):
-    # session_key = models.CharField(max_length=128, blank=True, default='')
     name = models.CharField(max_length=128, blank=True, default='')
     email = models.EmailField(blank=True, default='')
     user_apikey = models.CharField(max_length=128, blank=True, default='')
-    # name = models.CharField(max_length=128, blank=True, null=True, default=None)
-    # email = models.EmailField(blank=True, null=True, default=None)
-    # user_apikey = models.CharField(max_length=128, blank=True, null=True, default=None)
 
     class Meta:
         verbose_name = 'subscriber'
